1dc31629cae6ae6123ab4ca14a5e19d9448f0188
Shirhan <shirhan25@hotmail.com>
2022-03-08 23:05:18 +0000

    images of mission patch

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //declare the variable, an array of flights
 //let variables can not be redefined & have block scope.
-let flights = []; 
+let flights = [];
 
 //fetch() method requests to the server and load the information in the webpages. 
 //The request can be of any APIs that returns the data of the format JSON or XML.
@@ -12,101 +12,134 @@ let flights = [];
 //loadData method taking in no arguments 
 const loadData = () => {
     fetch("https://api.spacexdata.com/v5/launches")
-    //we got back a promise (data on flight)
+        //we got back a promise (data on flight)
 
-.then(result => result.json())
-//taking the resolved promise(the result from that fetch) and taking the json data from the result.
+        .then(result => result.json())
+        //taking the resolved promise(the result from that fetch) and taking the json data from the result.
 
-.then(data => flights = data)
-//taking the result.json data ? Our empty array of flights equals the data.
+        .then(data => flights = data)
+        //taking the result.json data ? Our empty array of flights equals the data.
 
-.then (() => { 
+        .then(() => {
 
-//MAKING FLIGHT DETAIL TO APPEAR FROM OUR API TO OUR DOMO 
+            //MAKING FLIGHT DETAIL TO APPEAR FROM OUR API TO OUR DOMO 
 
-console.log("Flight details loading ..." );
-//taking in no parameters but run everything defined inside of the function when resolved 
-const list = document.querySelector("#flight-detail-list")
-// declaring a variable called list and searching the document for details on the flight detail list id
-
-
-//currently our array of flights contains the json data of all the flights, we want to create a new array of just flight details 
-// we're gonna store the result of this in flightDetails
-// we're doing .map will iretate through each item in the array and allow us to transform each item. 
-//the result will be another item with the transformed items.
-// inside of .map (each flight(parameter) want to return the details)
-//map is taking a callback function 
-const flightDetails= flights.map(flight => {
-return flight.details;
-})
- 
-// flight details has an array of flight details now 
-//we want loop around the array of flight details using for each
-//each item is the details
-flightDetails.forEach((details) => {
-
-//create a variable to a list in the document 
-const listDetails= document.createElement("li")
+            console.log("Flight details loading ...");
+            //taking in no parameters but run everything defined inside of the function when resolved 
+            const list = document.querySelector("#flight-detail-list")
+            // declaring a variable called list and searching the document for details on the flight detail list id
 
 
-//add text(details in the array) in the document but attach to the list element created
-listDetails.innerText = details; 
+            //currently our array of flights contains the json data of all the flights, we want to create a new array of just flight details 
+            // we're gonna store the result of this in flightDetails
+            // we're doing .map will iretate through each item in the array and allow us to transform each item. 
+            //the result will be another item with the transformed items.
+            // inside of .map (each flight(parameter) want to return the details)
+            //map is taking a callback function 
+            const flightDetails = flights.map(flight => {
+                return flight.details;
+            })
 
-//adding the list of flight details at the end of the list in our document 
-list.appendChild(listDetails)
+            // flight details has an array of flight details now 
+            //we want loop around the array of flight details using for each
+            //each item is the details
+            flightDetails.forEach((details) => {
 
-})
-
-//we want catch to be called if our promise is rejected & the error to be caught.
-
-
-
-//Making a link to an article about the launch appear in our document 
-}).then(() => {
-
-    console.log("Flight article link loading ...");
-    const list = document.querySelector("#flight-article-link")
-
-    //we want to access the array of flights and use map to transform the items in the array by getting the 
-    // flight article links in a new array called articleList
- const articleList = flights.map(flight => {
-     return flight.links.article 
- })
+                //create a variable to a list in the document 
+                const listDetails = document.createElement("li")
 
 
-//loop around the aritclelist array and for each item (article), we want to :
-//create a list element in our document 
-//then put in the text of our item (article) as the newly created list
-//then append the list to the end of the list we made in our document
- 
-articleList.forEach((article ) => {
+                //add text(details in the array) in the document but attach to the list element created
+                listDetails.innerText = details;
 
-const listArticle = document.createElement("li");
+                //adding the list of flight details at the end of the list in our document 
+                list.appendChild(listDetails)
 
-listArticle.innerText = article;
+            })
 
-list.appendChild(listArticle)
-
-})
+            //we want catch to be called if our promise is rejected & the error to be caught.
 
 
 
-}).then(() => {
- console.log("Date of Flight loading ...");
+            //Making a link to an article about the launch appear in our document 
+        }).then(() => {
 
-const list = document.querySelector("#flight-local-date")
+            console.log("Flight article link loading ...");
+            const list = document.querySelector("#flight-article-link")
 
-const localDateList = flights.map (flight => {
-    return flight.date_local})
+            //we want to access the array of flights and use map to transform the items in the array by getting the 
+            // flight article links in a new array called articleList
+            const articleList = flights.map(flight => {
+                return flight.links.article
+            })
 
-    localDateList.forEach((date) => {
-const listDate = document.createElement("li")
-listDate.innerText = date;
-list.appendChild(listDate)
-})
-})
 
-/// Making the dates of the launches appear in our document 
+            //loop around the aritclelist array and for each item (article), we want to :
+            //create a list element in our document 
+            //then put in the text of our item (article) as the newly created list
+            //then append the list to the end of the list we made in our document
+
+            articleList.forEach((article) => {
+
+                const listArticle = document.createElement("li");
+
+                listArticle.innerText = article;
+
+                list.appendChild(listArticle)
+
+            })
+
+
+            /// Making the dates of the launches appear in our document 
+        }).then(() => {
+            console.log("Date of Flight loading ...");
+
+            const list = document.querySelector("#flight-local-date")
+
+            const localDateList = flights.map(flight => {
+                return flight.date_local
+            })
+
+            localDateList.forEach((date) => {
+                const listDate = document.createElement("li")
+                listDate.innerText = date;
+                list.appendChild(listDate)
+            })
+
+            //Adding image of the patches
+        }).then(() => {
+            console.log("Image of patch loading...");
+
+            //const image = document.querySelector("#flight-patch-image")
+
+            const list = document.querySelector("#flight-patch-image");
+            const imageList = flights.map(flight => {
+                return flight.links.patch.small
+            });
+
+            imageList.forEach((image) => {
+                const patchImage = document.createElement("img")
+
+                // make the image created the image item in the array
+                // in the json data we have the url of the image  which is the img src
+                patchImage.src = image
+
+                list.appendChild(patchImage);
+            })
+
+
+        })
+
+    //.catch((error) => { console.log( "Can't find image")});
+
+
+//format code
+//command + a = select all 
+//command + k =  format
+//command + f = format 
+
+
+
 
 }
 
